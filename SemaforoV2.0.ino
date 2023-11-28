53c9052c51d821b3fcdc1f171721dced4834b34a
Marco Acevedo <80277847+marco-acevedo-official@users.noreply.github.com>
2023-11-27 21:15:49 -0400

    Pedestrian Blinking

--- SemaforoV2.0.ino
+++ SemaforoV2.0.ino
@@ -1,28 +1,29 @@
-/* Laboratoy No 2 trafic light with pedestrian Light
-  Marco Antonio Acevedo Gonzalez  
-  Built on Arduino UNO */  
+/****************************************************/
+/* Laboratoy No 2 trafic light with pedestrian Light*/
+/*  Marco Antonio Acevedo Gonzalez    */
+/****************************************************/  
  
 //Stoplight A Pin number definitions
 #define stA_red 12
 #define stA_yellow 11
 #define stA_green 10
 #define crossA 9 //White LED that allows pedestrians to crossA
-#define buttonA 2 //Connected to Pin 2 and Ground
+#define buttonA 2
 
 //Stoplight B Pin number definitions
 #define stB_red 8
 #define stB_yellow 7
 #define stB_green 6
 #define crossB 5 //Blue LED that allows pedestrians to crossB
-#define buttonB 3 //Connected to Pin 3 and Ground
+#define buttonB 3
 
 //Global Delay is 1s
 #define Delay_time 1000
 
 //This value is multiplied by the global delay
 #define green_time 4 
-#define yellow_time 4
-#define red_time 4
+#define yellow_time 2
+#define red_time 3
 
 
 //Keeps Track of the current Cycle and Pedestrian Flags
@@ -61,7 +62,7 @@ void setup() {
   global_state = 0;
 }
 
-void pedAoff(){ //Turns off White LED for pedestrian A
+void pedAoff(){ //Turns White LED for pedestrian A
   digitalWrite(crossA,LOW);
 }
 
@@ -88,36 +89,31 @@ void doByState(){// Desiscion making function. Decides what to do based on the g
       }
       break;
     }
-      case 1:{//All stoplights are turned Red
-        allRed();
-        delay(Delay_time * red_time);
-        break;
-      }
-  //-----------------------------------------------------------------------
+    case 1:{//All stoplights are turned Red
+      allRed();
+      delay(Delay_time * red_time);
+      break;
+    }
     case 2:{//Stoplight A Green
       stAsetGreen();
       delay(Delay_time * green_time);
         break;
     }
-  //-----------------------------------------------------------------------    
     case 3:{//Stoplight A Yellow
       stAsetYellow();
       delay(Delay_time * yellow_time);
       break;
     }
-    
     case 4:{//Stoplight A Red
       allRed();
       delay(Delay_time * red_time);
       break;
     }
-  
     case 5:{//Stoplight B Green
       stBsetGreen();
       delay(Delay_time * green_time);
       break;
     }
-
     case 6:{//Stoplight B Yellow
       stBsetYellow();
       delay(Delay_time * yellow_time);
@@ -137,23 +133,18 @@ void doByState(){// Desiscion making function. Decides what to do based on the g
                      global_state++;
                      break;
                 }
-
-
          }//Only 1 pedestrian waiting else end
-
       }else{//No pedestrian waiting. This else is for the first If statement
         global_state = 0;
       }
       break;
     }// End bracket for Case 7
-
     case 8:{//Pedestrian A Waiting
       if(FlagA ==0){break;}//Skips this routine of there are no Pedestrian A waiting
      
       FlagA = 0;
-      pedAon();
-      delay(Delay_time*5);
-      pedAoff();
+
+      PedAroutine();
      
       //Checks to see if pedestrian B is also waiting
       if(FlagB == 1){break;}
@@ -163,9 +154,9 @@ void doByState(){// Desiscion making function. Decides what to do based on the g
 
     case 9:{//Pedestrian B Waiting
       FlagB = 0;
-      pedBon();
-      delay(Delay_time*5);
-      pedBoff();
+
+      PedBroutine();
+
       global_state = 0;
       break;
     }
@@ -241,6 +232,40 @@ void allRed(){//All stoplights are set to Red
 void allOff(){//All stoplights are set to off
   stAoff();
   stBoff();
+}
+
+void PedAroutine(){
+      //Turn on cross light and hold
+      pedAon();
+      delay(Delay_time*5);
+
+      //Start blinking the crossing light to warn time is over
+      for(i=0;i<5;i++){
+        pedAoff();
+        delay(250);
+        pedAon();
+        delay(250);
+      }
+
+      //Turn off pedestrian crossing light
+      pedAoff();
+}
+
+void PedBroutine(){
+      //Turn on cross light and hold
+      pedBon();
+      delay(Delay_time*5);
+
+      //Start blinking the crossing light to warn time is over
+      for(i=0;i<5;i++){
+        pedBoff();
+        delay(250);
+        pedBon();
+        delay(250);
+      }
+
+      //Turn off pedestrian crossing light
+      pedBoff();
 }
    
 void loop() {

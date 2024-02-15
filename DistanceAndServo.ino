e2c846b903009fd5c70130811ce1b9a103fb2cb7
Marco Acevedo <80277847+marco-acevedo-official@users.noreply.github.com>
2024-02-15 17:50:30 -0400

    Add files via upload

---
+++ DistanceAndServo.ino
@@ -0,0 +1,34 @@
+#define servo_pin 9
+#define TRIG_PIN 6
+#define ECHO_PIN 13
+#include <Servo.h>
+#include <SR04.h>
+SR04 sensor = SR04(ECHO_PIN,TRIG_PIN);
+Servo myservo;
+int angle;
+bool updateServo=false;
+long a;
+void setup() {
+  Serial.println("Setup");
+  myservo.attach(9);
+  Serial.begin(9600);
+  myservo.write(90);
+  angle=0;
+}
+void loop(){
+  a=sensor.DistanceAvg();
+  delay(100);
+  while(a<30){
+    updateServo=true;
+    angle=0;
+    moveServo();
+    a=sensor.DistanceAvg();
+    delay(100);
+  }
+  angle=90;
+  if(updateServo){moveServo();updateServo=false;}
+}
+
+void moveServo(){
+  myservo.write(angle);
+}
